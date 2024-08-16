4f5cadee5de447d1eca762dfa4c5f16c6cb7b151
alexjoabraham <alexjoabraham2@gmail.com>
2024-08-16 11:55:10 -0400

    changes to work in android emulator 2

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -31,11 +31,11 @@ class DetailScreen extends StatelessWidget {
           ),
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Row(
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Expanded(
-                  flex: 1,
+                Container(
+                  height: 200.0, // Adjust this value to control the image height
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(15.0),
                     child: AspectRatio(
@@ -43,84 +43,79 @@ class DetailScreen extends StatelessWidget {
                       child: Image.asset(
                         product.image,
                         fit: BoxFit.cover,
-                        height: 300,
                         width: double.infinity,
                       ),
                     ),
                   ),
                 ),
-                SizedBox(width: 16.0),
-                Expanded(
-                  flex: 1,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        product.name,
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xFF003B72),
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        '\$${product.price.toStringAsFixed(2)}',
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Color(0xFF003B72),
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      Flexible(
-                        child: Text(
-                          product.longDesc,
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.grey[700],
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      Flexible(
-                        child: Row(
-                          children: [
-                            IconButton(
-                              onPressed: () {
-                                Provider.of<CartProvider>(context, listen: false)
-                                    .addItem(product);
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                    content: Text('${product.name} added to cart!'),
-                                    duration: Duration(seconds: 2),
-                                    backgroundColor: Colors.green,
-                                  ),
-                                );
-                              },
-                              icon: Icon(Icons.add_shopping_cart),
-                              color: Color(0xFF003B72),
-                              iconSize: 32.0,
-                            ),
-                            SizedBox(width: 10),
-                            IconButton(
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => CartScreen(),
-                                  ),
-                                );
-                              },
-                              icon: Icon(Icons.shopping_cart),
-                              color: Color(0xFF003B72),
-                              iconSize: 32.0,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
+                SizedBox(height: 16.0),
+                Text(
+                  product.name,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF003B72),
                   ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  '\$${product.price.toStringAsFixed(2)}',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Color(0xFF003B72),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  product.longDesc,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey[700],
+                  ),
+                  textAlign: TextAlign.center, // Optional: centers the text
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Provider.of<CartProvider>(context, listen: false)
+                        .addItem(product);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('${product.name} added to cart!'),
+                        duration: Duration(seconds: 2),
+                        backgroundColor: Colors.green,
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF003B72),
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                  child: Text('Add to Cart'),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CartScreen(),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF003B72),
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                  child: Text('View Cart'),
                 ),
               ],
             ),
